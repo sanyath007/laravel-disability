c2fa7daba823fbb5df778ed03504afac7974c436
sanyath007 <sanyath007@gmail.com>
2022-03-23 14:31:00 +0700

    Create new setDisabilities and getDataWithUrl methods in disabilityController

--- public/js/controllers/disabilityController.js
+++ public/js/controllers/disabilityController.js
@@ -2,6 +2,7 @@ app.controller('disabilityController', function($scope, $http, CONFIG) {
     $scope.cboYear = parseInt(moment().format('YYYY')) + 543;
     $scope.disabilities = [];
     $scope.pager = null;
+    $scope.loading = false;
 
     const initDateMonthPicker = {
         autoclose: true,
@@ -30,14 +31,32 @@ app.controller('disabilityController', function($scope, $http, CONFIG) {
     $scope.search = function(year = '') {
         $http.get(`${CONFIG.baseUrl}/disabilities/list?year=${year}`)
         .then(res => {
-            console.log(res);
-            const { data, ...pager } = res.data.disabilities;
-            $scope.disabilities = data;
-            $scope.pager = pager;
-
-            console.log($scope.disabilities);
+            $scope.setDisabilities(res);
         }, err => {
             console.log(err);
         });
-    }
+    };
+
+    $scope.setDisabilities = function(res) {
+        const { data, ...pager } = res.data.disabilities;
+        $scope.disabilities = data;
+        $scope.pager = pager;
+    };
+
+    $scope.getDataWithUrl = function(e, URL, cb) {
+        /** Check whether parent of clicked a tag is .disabled just do nothing */
+        if ($(e.currentTarget).parent().is('li.disabled')) return;
+
+        $scope.loading = true;
+
+        $http.get(URL)
+        .then(function(res) {
+            cb(res);
+
+            $scope.loading = false;
+        }, function(err) {
+            console.log(err);
+            $scope.loading = false;
+        });
+    };
 });
--- resources/views/disability/_list.blade.php
+++ resources/views/disability/_list.blade.php
@@ -83,19 +83,19 @@
                         <div class="col-md-4">
                             <ul class="pagination pagination-sm no-margin pull-right" ng-show="pager.last_page > 1">
                                 <li ng-if="pager.current_page !== 1">
-                                    <a href="#" ng-click="getDataWithURL($event, pager.path+ '?page=1', setLeaves)" aria-label="Previous">
+                                    <a href="#" ng-click="getDataWithUrl($event, pager.path+ '?page=1', setDisabilities)" aria-label="Previous">
                                         <span aria-hidden="true">First</span>
                                     </a>
                                 </li>
                             
                                 <li ng-class="{'disabled': (pager.current_page==1)}">
-                                    <a href="#" ng-click="getDataWithURL($event, pager.prev_page_url, setLeaves)" aria-label="Prev">
+                                    <a href="#" ng-click="getDataWithUrl($event, pager.prev_page_url, setDisabilities)" aria-label="Prev">
                                         <span aria-hidden="true">Prev</span>
                                     </a>
                                 </li>
 
                                 <!-- <li ng-repeat="i in debtPages" ng-class="{'active': pager.current_page==i}">
-                                    <a href="#" ng-click="getDataWithURL(pager.path + '?page=' +i)">
+                                    <a href="#" ng-click="getDataWithUrl(pager.path + '?page=' +i)">
                                         @{{ i }}
                                     </a>
                                 </li> -->
@@ -107,13 +107,13 @@
                                 </li> -->
 
                                 <li ng-class="{'disabled': (pager.current_page==pager.last_page)}">
-                                    <a href="#" ng-click="getDataWithURL($event, pager.next_page_url, setLeaves)" aria-label="Next">
+                                    <a href="#" ng-click="getDataWithUrl($event, pager.next_page_url, setDisabilities)" aria-label="Next">
                                         <span aria-hidden="true">Next</span>
                                     </a>
                                 </li>
 
                                 <li ng-if="pager.current_page !== pager.last_page">
-                                    <a href="#" ng-click="getDataWithURL($event, pager.path+ '?page=' +pager.last_page, setLeaves)" aria-label="Previous">
+                                    <a href="#" ng-click="getDataWithUrl($event, pager.path+ '?page=' +pager.last_page, setDisabilities)" aria-label="Previous">
                                         <span aria-hidden="true">Last</span>
                                     </a>
                                 </li>
